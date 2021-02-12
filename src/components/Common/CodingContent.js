52a4d5020a0f72c896beaf485d9ea7abcff6bc70
Kelsey Sheridan <kelsi0@live.com>
2021-02-12 15:00:17 +0000

    Made Trinket Bigger

--- src/components/Common/CodingContent.js
+++ src/components/Common/CodingContent.js
@@ -17,11 +17,11 @@ class CodingContent extends Component {
                     <div class="col-lg-12 mx-auto text-center"><p class="large text-muted">Having recently completed a three week Develop Coding course with CodeNation, I have begun a job search for a role in the industry, I have continued to learn more and begin new projects.</p></div>
                     <div class="col-lg-12 mx-auto text-center paddingTop">
                     <p class="large text-muted">Here is a small program that will provide you with my github or linkedin page, please hit the run button to play!</p>
-                        <iframe src="https://trinket.io/embed/python/2444963a5d" width="90%" height="300" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe> 
+                        <iframe src="https://trinket.io/embed/python/2444963a5d" width="90%" height="500" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe> 
                     </div>
                     <div class="col-lg-12 mx-auto text-center paddingTop">
                     <p class="large text-muted">During my course we had to create a small python game, we went with a different idea but this was my sample game!</p>
-                        <iframe src="https://trinket.io/embed/python/2c408193a3?outputOnly=true&runOption=run" width="90%" height="300" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
+                        <iframe src="https://trinket.io/embed/python/2c408193a3?outputOnly=true&runOption=run" width="90%" height="500" frameborder="0" marginwidth="0" marginheight="0" allowfullscreen></iframe>
                     </div>    
                 </div>
             </div>
