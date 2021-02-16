57ca97cbe66f98b88a57854695c57fedfdb50ccb
Kelsey Sheridan <kelsi0@live.com>
2021-02-16 15:59:10 +0000

    temp image fix

--- src/components/Common/CardComponent.js
+++ src/components/Common/CardComponent.js
@@ -1,15 +1,16 @@
 import React, {Component} from 'react';
-import image from "../assets/img/Ham.jpg";
+import Image from "../assets/img/Climbing/Climbing.jpg"
 
 class CardComponent extends Component {
     render(){
+        console.log(this.props.card_details.card_image);
         return(
         <div className="cardBox">
         <div className="col-md-12">
             <a className="my-3" href="/blogArticlePage">{this.props.title}</a>
             <h6 className="my-3">{this.props.date}</h6>
-            <img className="img-fluid smallImageBox" src={this.props.image} alt="Picture"></img>
-            <p className="text-muted">{this.props.summary}</p>
+            <img className="img-fluid smallImageBox" src={Image} alt="Picture"></img>
+            <p className="text-muted">{this.props.card_details.summary}</p>
         </div>
         </div>
         )
