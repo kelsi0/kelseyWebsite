a04d0f4092fd22fc922ecee03b387ac3bea935e4
kelsi0 <kelsi0@live.com>
2021-02-17 12:00:48 +0000

    Fixed the alt

--- src/components/Blog/ArticleComponentPicture.js
+++ src/components/Blog/ArticleComponentPicture.js
@@ -4,7 +4,7 @@ class ArticleComponentPicture extends Component {
     render(){
         return(
             <div>
-                <img className="img-fluid smallImageBox" src={this.props.post_elements.image_url} alt="Picture"></img>
+                <img className="img-fluid smallImageBox" src={this.props.post_elements.image_url} alt=""></img>
             </div>
         )
     }
